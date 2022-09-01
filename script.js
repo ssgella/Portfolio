39a1d0c28953f36b98e0936940f0e7f82192ceab
ssgella <ssgella@gmail.com>
2022-09-01 11:43:16 +0100

    Automatic light mode dark mode set up

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-var isDark = true
+var isDark = false
+
+const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+const userPrefersLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+
+function setMode() {
+    if (userPrefersDark) {
+        console.log('dark')
+        document.getElementById("modeSelector").value = "light_mode"
+        isDark = true
+        return(isDark)
+    } else if (userPrefersLight) {
+        console.log('light')
+        document.getElementById("modeSelector").value = "dark_mode"
+        isDark = false
+        return(isDark)
+    } else { console.log('error')}
+}
 function modeChange() {
     if (isDark) {
         document.getElementById("modeSelector").value = "dark_mode"
         document.body.classList.toggle("light-mode");
         isDark = false
+        console.log('lightmode')
         return(isDark)
     } else if (isDark === false) {
         document.getElementById("modeSelector").value = "light_mode"
         document.body.classList.toggle("light-mode");
         isDark = true
+        console.log('darkmode')
         return(isDark)
     } else {
         console.log("mode selector error")
     }
+}
+
+setMode()
+console.log(isDark)
+if (isDark) {
+
+} else {
+    modeChange()
 }
